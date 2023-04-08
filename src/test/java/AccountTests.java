b94f17849285b2e9c9c76ec94f42400b47fd4e42
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-04-08 11:43:05 -0300

    Project structure and deposit method test

---
+++ src/test/java/AccountTests.java
@@ -0,0 +1,21 @@
+import dm.mayolo.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests
+{
+    @Test
+    public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount()
+    {
+        // Arrange
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account account = new Account( 1L, 0.0 );
+
+        // Act
+        account.deposit( amount );
+
+        // Assert
+        Assertions.assertEquals( expectedValue, account.getBalance() );
+    }
+}
