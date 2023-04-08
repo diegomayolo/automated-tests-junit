42a93c1c840f4c185ed480ccf742cd1ea02d9ccb
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-04-08 11:54:27 -0300

    Deposit method with no return

--- src/test/java/AccountTests.java
+++ src/test/java/AccountTests.java
@@ -18,4 +18,19 @@ public class AccountTests
         // Assert
         Assertions.assertEquals( expectedValue, account.getBalance() );
     }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount()
+    {
+        // Arrange
+        double amount = -200.0;
+        double expectedValue = 0.0;
+        Account account = new Account( 1L, 0.0 );
+
+        // Act
+        account.deposit( amount );
+
+        // Assert
+        Assertions.assertEquals( expectedValue, account.getBalance() );
+    }
 }
